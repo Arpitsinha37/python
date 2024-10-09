1dbdf3e5d1589b41d36f66ac2351d86b3c3eda10
Arpit Sinha <arpitsinha579@gmail.com>
2024-10-09 22:31:55 +0530

    linked_list_matrix

---
+++ pair_sum.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def pairsum(self, arr : List[int]) -> int:
+        arp =max(arr)
+        return arp + max([atr for atr in arr if atr != arp])
+        
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        res = obj.pairsum(arr)
+
+        print(res)
