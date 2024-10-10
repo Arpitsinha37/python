a267d9f499cb3b9f245d5fb782524ef83e919376
Arpit Sinha <arpitsinha579@gmail.com>
2024-10-10 22:20:49 +0530

    max_distance_between_same_element

---
+++ max_distance_between_same_element.py
@@ -0,0 +1,22 @@
+class solution:
+   def maxDistance(self, arr):
+        # Code here
+        mp = {}
+        for i in range(len(arr)):
+            if arr[i] in mp:
+                mp[arr[i]] = [mp[arr[i]][0],i]
+            else:
+                mp[arr[i]] = [i,0]
+                # print(mp)
+                
+        mx = 0
+        for k,v in mp.items():
+            mx = max(v[1]-v[0],mx)
+        return mx
+    
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        print(ob.maxDistance(arr))
