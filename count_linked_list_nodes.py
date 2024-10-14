54ebe7b803bb4107562e7f1e265bce19df51d7cb
Arpit Sinha <arpitsinha579@gmail.com>
2024-10-14 19:44:02 +0530

    Create count_linked_list_nodes.py

---
+++ count_linked_list_nodes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def getCount(self, head):
+        temp=head
+        count=0
+        while temp.next:
+            count+=1
+            temp=temp.next
+        return count+1
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+
+    t = int(data[0])
+    for i in range(1, t + 1):
+        arr = list(map(int, data[i].split()))
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        ob = Solution()
+        print(ob.getCount(head))
