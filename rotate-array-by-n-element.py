d2cdc450e2d03ba5819b6d2799dbccea0a2606ef
Arpit Sinha <arpitsinha579@gmail.com>
2024-11-18 23:34:56 +0530

    Create rotate-array-by-n-element.py

---
+++ rotate-array-by-n-element.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        #Your code here
+        n=len(arr)
+        d=d%n
+        # print(d)
+        if d==0:
+            return arr
+        else:
+            # print(arr[d:n])
+            x=arr[d:n]+arr[:d]
+            for i in range(n):
+                arr[i]=x[i]
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+
+    while (T > 0):
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ob.rotateArr(A, D)
+
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
