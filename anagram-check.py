e5cf26fbaca06fc8bc4517bf28536542a60d84ed
Arpit Sinha <arpitsinha579@gmail.com>
2024-11-30 23:21:26 +0530

    Create anagram-check.py

---
+++ anagram-check.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def areAnagrams(self, s1, s2):
+        #code here
+        s1=list(s1)
+        s2=list(s2)
+        s1.sort()
+        s2.sort()
+        if len(s1)!=len(s2):
+            return False
+        for i in range(len(s1)):
+            if s1[i]!=s2[i]:
+                return False
+        return True
